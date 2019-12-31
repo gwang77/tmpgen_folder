11e573627ffedd585794e262e9ab5883e04f0e7c
wanggang@kedacom.com <wanggang@kedacom.com>
2019-12-31 13:32:23 +0800

    update

--- tmpgen/src/main/resources/tmpgen.bat
+++ tmpgen/src/main/resources/tmpgen.bat
@@ -1,4 +1,4 @@
 SET CLASSPATH=tmpgen.jar;
 for %%I in (dir ./lib/*.jar) do call cpappend.bat ./lib/%%I
 
-D:\Java\jdk1.8.0_131\bin\java.exe -classpath %CLASSPATH% TmpGen
+D:\Java\jdk1.8.0_131\bin\java.exe -Dfile.encoding=utf-8 -classpath %CLASSPATH% TmpGen
