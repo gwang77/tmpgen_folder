2feaab76dbd539e07e0b0e9d3b116ffeee36b224
wanggang <wanggang@kedacom.com>
2020-09-17 14:41:32 +0800

    update

--- tmpgen/src/main/template/src/main/java/com/${baseproject}/${project}/${module}/dao/${objectname}Repository.java
+++
@@ -1,9 +0,0 @@
-package com.${baseproject}.${project}.${module}.dao;
-
-import com.${baseproject}.kidp.base.data.common.repository.BaseJpaRepository;
-import com.${baseproject}.${project}.${module}.entity.${objectname};
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ${objectname}Repository extends BaseJpaRepository<${objectname}> {
-}
